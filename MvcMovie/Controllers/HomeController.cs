558ab4dd9adc20db9f414a843554698a4a4294cf
Oséas Francisco de Lemos André <oseas.lemos@fujb.ufrj.br>
2022-06-03 18:32:22 -0300

    First commit

---
+++ MvcMovie/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace MvcMovie.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Index(string searchString)
+        {
+            var movies = from m in _context.Movie select m;
+
+            if( !String.IsNullOrEmpty(searchString) )
+            {
+                movies = movies.Where( s => s.Title!.Contains(searchString) );
+            }
+
+            return View(await movies.ToListAsync());
+        }
+    }
+}
---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string? Tiltle { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string? Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
